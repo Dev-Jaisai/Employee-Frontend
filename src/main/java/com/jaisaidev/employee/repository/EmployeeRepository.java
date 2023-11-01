e9f364c009733ad56d05f265de449a81530c3f5a
Jaisai <jaisaisarkates0506@gmail.com>
2023-11-01 14:05:36 +0530

    first commit for Backend Employee management small project

---
+++ src/main/java/com/jaisaidev/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.jaisaidev.employee.repository;
+
+import com.jaisaidev.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
